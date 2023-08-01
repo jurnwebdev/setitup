1303658c7642037c3ed955e07ad7a1ecfa18d21f
Tobi John <connect@tobijohn.com>
2023-08-01 22:59:20 +0100

    updated the navbar menu

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <section className="banner w-full h-auto py-12 px-6 md:h-[450px] bg-blue-50 flex flex-col justify-center items-start md:items-center gap-2">
+      <section className="banner w-full h-auto py-12 px-8 md:h-[450px] bg-blue-50 flex flex-col justify-center items-start md:items-center gap-2">
         <h1 className="title text-[50px] leading-[50px] md:text-[60px] font-bold text-[#00203c]">Ready, Set, Download.</h1>
         <p className='text-[20px] md:text-[25px] font-light text-[#00203c]'>Tailor-Made Software for Your New Laptop</p>
         <button className='py-4 px-8 rounded hover:bg-[#6E97FD] text-white mt-4 bg-[#00203c]'>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,32 @@
+'use client'
+import { useState } from 'react'
 import React from 'react'
 import logo from '../public/setituplogo.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import NavBar from '../public/navBar.svg'
+import closeNav from '../public/close.svg'
 
 const Header = () => {
+    const [open, close] = useState(false)
+
+    const handleClick = () => {
+        close(!open)
+        console.log(close)
+    }
+
     return (
-        <header className='flex justify-between items-center px-3 md:px-10 py-4 bg-white'>
+        <header className='flex justify-between items-center px-6 relative md:px-10 py-4 bg-white'>
             <Image src={logo} width={200} height={50} alt="Set it Up logo" className='w-32'></Image>
-            <Image src={NavBar} width={40} height={40} alt='Navbar Icon' className='flex md:hidden'></Image>
+            <button onClick={handleClick}><Image src={open ? closeNav : NavBar} width={40} height={40} alt='Navbar Icon' className='flex md:hidden'></Image></button>
+            <div className={`mobile-menu-container absolute bg-white p-10 w-full duration-300 ${open ? 'flex top-[75px]' : 'hidden top-[-200px]'}`}>
+                <ul className='flex flex-col gap-6 items-start title-color text-[1.3em]'>
+                    <li className='hover:text-[#7096ff] hover:translate-y-2 duration-300 ease-in'><Link href='#'>Discover.</Link></li>
+                    <li className='hover:text-[#7096ff] hover:translate-y-2 duration-300 ease-in'><Link href='#'>Mac Os.</Link></li>
+                    <li className='hover:text-[#7096ff] hover:translate-y-2 duration-300 ease-in'><Link href='#'>Windows Os.</Link></li>
+                    <li className='hover:text-[#7096ff] hover:translate-y-2 duration-300 ease-in'><Link href='#'>Contact.</Link></li>
+                </ul>
+            </div>
             <nav className='hidden md:flex'>
                 <ul className='flex gap-6 items-center title-color'>
                     <li className='hover:text-[#7096ff] hover:translate-y-2 duration-300 ease-in'><Link href='#'>Discover.</Link></li>
@@ -18,7 +36,7 @@ const Header = () => {
                 </ul>
 
             </nav>
-        </header>
+        </header >
     )
 }
 
